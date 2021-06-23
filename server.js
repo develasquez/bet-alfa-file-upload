07274ab4731f15fbbab2068da01be6368b8fb4b6
Felipe Velasquez <felipevelasquez@MacBook-Air-de-Felipe.local>
2021-06-23 16:51:19 -0400

    Allow Cors

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ let corsOptions = {
   origin: "*",
 };
 
-app.use(cors(corsOptions));
+app.use(
+(app, req, res, next) => {
+res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+    app.options('*', (req, res) => { 
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
+  })
 
 const initRoutes = require("./src/routes");
 
